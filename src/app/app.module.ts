b0dca6f404b44e4aef5c27d2da70dfeb73a6747e
mohsenrezaei5367 <mohsenrezaei5367@gmail.com>
2022-03-04 15:36:33 +0330

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './share/header/header.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { LayoutComponent } from './share/layout/layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    LayoutComponent,
+  
   ],
   imports: [
     BrowserModule,
